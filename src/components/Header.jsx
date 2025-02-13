f4076a9951cc1d8d6ecc988f0bc81998c3137456
Leon <leonissaclivi@gmail.com>
2025-02-14 00:51:53 +0530

    removed a tag in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand >MovieReviewer.site</Navbar.Brand>
+        <Navbar.Brand>MovieReviewer.site</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
